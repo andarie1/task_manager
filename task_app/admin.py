da896e3dafd6e62e07cc8cc21d785f137bd57b46
andarie1 <elena.arhi88@gmail.com>
2025-02-23 21:00:21 +0200

    Added action, inlines, shot title

--- task_app/admin.py
+++ task_app/admin.py
@@ -6,17 +6,31 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
 
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at')
-    list_filter = ('status', 'deadline')
-    search_fields = ('title', 'description')
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1
+    min_num = 0
+    can_delete = True
+
+@admin.action(description="Mark as 'Done'")
+def mark_as_done(self, request, queryset):
+    queryset.update(status="done")
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'task', 'status', 'deadline')
     list_filter = ('status',)
     search_fields = ('title', 'description')
+    actions = [mark_as_done]
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'created_at')
+    list_filter = ('status', 'deadline')
+    search_fields = ('title', 'description')
+    inlines = [SubTaskInline]
+
+
 
 
 
